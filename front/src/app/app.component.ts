ee759e0c85ab82bee139b9496b9758c7678d8baa
Weyrd <wyrd.reisa@gmail.com>
2023-05-23 08:59:41 +0200

    css master :sunglass:

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -9,59 +9,9 @@ import {DragAndDropComponent} from "./drag-and-drop/drag-and-drop.component";
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'front';
   protected readonly ImagesHandlerService = ImagesHandlerService;
 
-  generateRandomGraph(): void {
-    const graphContainer = d3.select('#graph');
-
-    // Clear any existing graph
-    graphContainer.selectAll('*').remove();
-
-    // Generate random data for the graph
-    const nodes = Array.from({length: 10}, (_, i) => ({id: i}));
-    const links = Array.from({length: 10}, () => ({
-      source: Math.floor(Math.random() * 10),
-      target: Math.floor(Math.random() * 10),
-    }));
-
-    // Set up the graph layout
-    const width = 400;
-    const height = 300;
-    const svg = graphContainer
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    // Create the graph elements
-    const linkElements = svg
-      .selectAll('line')
-      .data(links)
-      .enter()
-      .append('line')
-      .attr('stroke', 'gray')
-      .attr('stroke-width', 2);
-
-    const nodeElements = svg
-      .selectAll('circle')
-      .data(nodes)
-      .enter()
-      .append('circle')
-      .attr('r', 10);
-
-    console.log("nodeElements", nodeElements)
-  }
-
-
-  ngOnInit(): void {
-    // load drag and drop component
-    const dragAndDropComponent = new DragAndDropComponent();
-    dragAndDropComponent.onDrop(new DragEvent("drop"));
-
-    // wait for image is uploaded, generate graph
-
-
-  }
 }
 
